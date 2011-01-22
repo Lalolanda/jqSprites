0c8055fc5073b1e46e32cd947be5690403463c6e
Roque Terrani <riterani@gmail.com>
2011-01-22 09:23:07 -0300

    First commit with code and some sprite sheets

---
+++ index.php
@@ -0,0 +1,40 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>jTimer</title>
+	<script type="text/javascript" src="jquery-1.4.4.min.js"></script>
+	<script type="text/javascript" src="jquery.ui.widget.js"></script>
+	<script type="text/javascript" src="jSprites.js"></script>
+	<style>
+		.mario{
+			width:30px;
+			height:32px;
+			background:url('mario.gif');
+			position:absolute;
+			top:0px;
+			left:0px;
+		}
+		.toad{
+			width:32px;
+			height:48px;
+			background:url('toad.gif');
+			position:absolute;
+			top:0px;
+			left:50px;
+		}
+	</style>
+	<script>
+		var sd;
+		$(document).ready(function(){
+			
+		});
+	</script>
+</head>
+
+<body>
+
+</body>
+</html>
