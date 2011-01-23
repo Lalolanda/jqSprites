dd2dd9e916dd38b5f182f5827a07b41646cbd88c
Roque Terrani <riterani@gmail.com>
2011-01-23 10:29:31 -0300

    Writed the basic classes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<title>jTimer</title>
 	<script type="text/javascript" src="jquery-1.4.4.min.js"></script>
 	<script type="text/javascript" src="jquery.ui.widget.js"></script>
-	<script type="text/javascript" src="jSprites.js"></script>
+	<script type="text/javascript" src="jqSprites.js"></script>
 	<style>
 		.mario{
 			width:30px;
@@ -31,10 +31,11 @@
 		$(document).ready(function(){
 			
 		});
+		
 	</script>
 </head>
 
 <body>
-
+<canvas id="canvas"></canvas>
 </body>
 </html>
